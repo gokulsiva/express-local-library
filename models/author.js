f1bd08a16352c6ecd506629dd6123dfa1c230a7b
gokulsiva <gsgokul305@gmail.com>
2019-06-12 00:37:05 +0530

    all list and details page added.

--- models/author.js
+++ models/author.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
+var moment = require('moment');
 
 var Schema = mongoose.Schema;
 
@@ -30,6 +31,20 @@ AuthorSchema
 .virtual('url')
 .get(function () {
   return '/catalog/author/' + this._id;
+});
+
+// Virtual for author's formatted date of birth
+AuthorSchema
+.virtual('date_of_birth_formatted')
+.get(function () {
+  return moment(this.date_of_birth).format('MMMM Do, YYYY');
+});
+
+// Virtual for author's formatted date of death
+AuthorSchema
+.virtual('date_of_death_formatted')
+.get(function () {
+  return moment(this.date_of_death).format('MMMM Do, YYYY');
 });
 
 //Export model
