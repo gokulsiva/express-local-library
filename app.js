a17e69709554282848cb7e3fb9cbd394257993df
gokulsiva <gsgokul305@gmail.com>
2019-06-15 19:22:05 +0530

    dotenv added to package and used in app

--- app.js
+++ app.js
@@ -3,10 +3,14 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var dotenv = require('dotenv');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var catalogRouter = require('./routes/catalog');
+
+// Configure the environment
+dotenv.config();
 
 var app = express();
 
